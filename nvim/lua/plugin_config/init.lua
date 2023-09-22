1f7885616f1486f669c8855d3dd412f015544c79
Markus <51286688+markusfossdal@users.noreply.github.com>
2023-09-22 14:49:55 -0700

    added nvim functionality

--- nvim/lua/plugin_config/init.lua
+++ nvim/lua/plugin_config/init.lua
@@ -1,6 +1,7 @@
 require('plugin_config.gruvbox')
 require('plugin_config.lualine')
-require('plugin_config.nvimtree')
+--require('plugin_config.nvimtree')
 require('plugin_config.treesitter')
 require('plugin_config.telescope')
 require('plugin_config.lsp_config')
+require('plugin_config.completions')
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -9,43 +9,57 @@ local ensure_packer = function()
   return false
 end
 
+
 local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-  -- My plugins here
-  use { "ellisonleao/gruvbox.nvim" } -- Colorscheme
-
-  use {
-      'nvim-lualine/lualine.nvim',
-      requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-  }
-
-  use 'nvim-tree/nvim-tree.lua'
-  use 'nvim-tree/nvim-web-devicons'
-  use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use {
-      'nvim-telescope/telescope.nvim',
-      tag = '0.1.0',
-      requires = { {'nvim-lua/plenary.nvim'} }
-  }
-
-  use {
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
-      "neovim/nvim-lspconfig",
-  }
+    --  Packer
+    use 'wbthomason/packer.nvim'
+    -- Colorscheme
+    use { "ellisonleao/gruvbox.nvim" } -- Colorscheme
 
 
-  -- undo tree?
-  -- fugitive?
-  -- harpoon?
-  -- cmd for auto completion
+    -- Lualine
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
+    -- nvim-tree
+    --  use 'nvim-tree/nvim-tree.lua'
+    --  use 'nvim-tree/nvim-web-devicons'
+
+    -- Treesitter
+    use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.0',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+
+    -- LSP
+    use {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+    }
+
+    -- nvim-cmd
+    use {
+        'hrsh7th/nvim-cmp',
+        'hrsh7th/cmp-nvim-lsp',
+        'L3MON4D3/LuaSnip',
+        'saadparwaiz1/cmp_luasnip',
+        'rafamadriz/friendly-snippets'
+    }
+    -- undo tree?
+    -- fugitive?
+    -- harpoon?
+    -- cmd for auto completion
 
 
--- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
